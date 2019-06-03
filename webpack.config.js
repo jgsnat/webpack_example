a19306fab119228d4b590c1cf53d7ccdb0c270ba
Jefferson Silva <jgsnat@gmail.com>
2019-06-03 13:34:48 -0300

    - first setup;

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 3000,
+        contentBase: './public'
+    }
+}
