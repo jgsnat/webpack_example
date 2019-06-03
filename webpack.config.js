81f6ad91c1a772596517302505ed29aa6fe19de1
Jefferson Silva <jgsnat@gmail.com>
2019-06-03 17:43:02 -0300

    - webpack with babel;

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,29 @@
 const webpack = require('webpack');
 
 module.exports = {
+
     entry: './src/index.js',
+
     output: {
         path: __dirname + '/public',
         filename: './bundle.js'
     },
+
     devServer: {
         port: 3000,
         contentBase: './public'
+    },
+
+    module: {
+        rules: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        ]
     }
 }
