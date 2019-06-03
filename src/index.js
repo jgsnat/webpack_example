a67abbbdcc393364e8658ee08f7464056a6d98ff
Jefferson Silva <jgsnat@gmail.com>
2019-06-03 13:51:16 -0300

    - ES5 without Babel;

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const logger = require('./logger');
+import Pessoa from './models/pessoa';
 
 console.log('Webpack');
 logger.info('Usuando o padrão CommonJS!');
+
+const pessoa = new Pessoa('Jefferson');
+logger.info(pessoa.toString());
